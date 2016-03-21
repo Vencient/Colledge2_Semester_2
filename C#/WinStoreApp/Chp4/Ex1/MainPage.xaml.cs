89489d044f0abdc6f9ffe12356b9f04aa1673616
unknown <>
2016-03-21 21:04:55 +0800

    add MainPage

---
+++ C#/WinStoreApp/Chp4/Ex1/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace WSAD_Chp4_Ex1
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+
+            this.InitializeComponent();
+            lbNoteName.ItemsSource = NotesCollection.Notes;
+            if (NotesCollection.Notes.Count != 0)
+                txtNoNotes.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(AddNotePage));
+        }
+
+        private void lbNoteName_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Frame.Navigate(typeof(DetailPage), lbNoteName.SelectedItem);
+        }
+    }
+}
---
+++ C#/WinStoreApp/Chp4/Ex1/Note.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WSAD_Chp4_Ex1
+{
+    class NotesCollection
+    {
+        public static ObservableCollection<Note> Notes = new ObservableCollection<Note>();
+    }
+    class Note
+    {
+        string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        string content;
+
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        public Note(string Title, string Content)
+        {
+            title = Title;
+            content = Content;
+        }
+        public override string ToString()
+        {
+            return Title +": "+ Content;
+        }
+    }
+}
